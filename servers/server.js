0b6baa6f2435c9a1bde90f51ec86f7c04ace5181
Kanhaiya Mishra <knhmishra996@gmail.com>
2022-12-09 02:06:22 +0530

    Update server.js

--- servers/server.js
+++ servers/server.js
@@ -23,12 +23,11 @@ mongoose
   if(process.env.NODE_ENV === 'production'){    
     app.use(express.static('client/build'))  // set static folder 
     //returning frontend for any route other than api 
-    app.get('*',(req,res)=>{     
+    app.get('/^((?!(api)).)*$/',(req,res)=>{     
         res.sendFile (path.resolve(__dirname,'client','build',         
                       'index.html' ));    
     });
 }
-
 
 
 app.use("/dev",dev);
